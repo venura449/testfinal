1110750cc71982499a3be71c81e42903c28427a4
Member1 <thisalseniye@gmail.com>
2026-03-01 19:57:44 +0530

    Fix logic in CampusResourceRepository.java

--- backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
+++ backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
@@ -5,3 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CampusResourceRepository extends MongoRepository<CampusResource, String> {
 }
+
+// refactored: 2026-03-01T19:57:44
