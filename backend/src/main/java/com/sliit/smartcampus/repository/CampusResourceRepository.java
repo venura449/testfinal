8dc4bf9897ddcad47286e11dc4a84285a39bcb5a
Member1 <thisalseniye@gmail.com>
2026-03-16 20:07:02 +0530

    Complete CampusResourceRepository.java

---
+++ backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
@@ -0,0 +1,7 @@
+package com.sliit.smartcampus.repository;
+
+import com.sliit.smartcampus.entity.CampusResource;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CampusResourceRepository extends MongoRepository<CampusResource, String> {
+}
