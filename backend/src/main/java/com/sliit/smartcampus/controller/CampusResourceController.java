1d69bda675a9b9a669dc5c913107263e9b967568
Member1 <thisalseniye@gmail.com>
2026-04-14 15:15:13 +0530

    Add CampusResourceController.java

--- backend/src/main/java/com/sliit/smartcampus/controller/CampusResourceController.java
+++ backend/src/main/java/com/sliit/smartcampus/controller/CampusResourceController.java
@@ -30,7 +30,7 @@ public class CampusResourceController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('ADMIN')")
     public ResourceResponse create(@RequestBody ResourceRequest request) {
         return campusResourceService.create(request);
