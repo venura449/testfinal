0ac90037bb65885807333760e2fcf497789d76ea
Member3 <lansakara1020@gmail.com>
2026-04-21 13:10:55 +0530

    Add FileStorageService.java

--- backend/src/main/java/com/sliit/smartcampus/dto/maintenance/TicketCommentRequest.java
+++ backend/src/main/java/com/sliit/smartcampus/dto/maintenance/TicketCommentRequest.java
@@ -3,7 +3,7 @@ package com.sliit.smartcampus.dto.maintenance;
 public record TicketCommentRequest(String content) {
 }
 
-// refactored: 2032-04-12T14:29:17
+// refactored: 2033-04-12T14:29:17
 
 // validated: 2026-04-23T09:24:48
 
