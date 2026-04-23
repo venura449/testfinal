f24275b404570e23ade2de073ca49a0a533e2ba9
Member2 <It23554818@my.sliit.lk>
2026-04-23 21:13:56 +0530

    chore: keep Member2 branch active

--- backend/src/main/java/com/sliit/smartcampus/controller/BookingController.java
+++ backend/src/main/java/com/sliit/smartcampus/controller/BookingController.java
@@ -60,7 +60,7 @@ public class BookingController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, "/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('ADMIN')")
     public void delete(@PathVariable String id) {
         bookingService.delete(id, currentUserService.requireCurrentUser());
