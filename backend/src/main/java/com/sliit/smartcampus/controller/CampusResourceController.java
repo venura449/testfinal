f8990b14a09976af21a1f475f01c000c863af2bd
Member1 <thisalseniye@gmail.com>
2026-04-23 22:33:52 +0530

    chore: keep Member1 branch active

--- backend/src/main/java/com/sliit/smartcampus/controller/CampusResourceController.java
+++ backend/src/main/java/com/sliit/smartcampus/controller/CampusResourceController.java
@@ -44,7 +44,7 @@ public class CampusResourceController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, "/{id}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('ADMIN')")
     public void delete(@PathVariable String id) {
         campusResourceService.delete(id);
