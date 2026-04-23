33b1cb585cbeb7032c9adb038c592fe3013f65f9
Member4 <thimirathathsarana2003@gmail.com>
2026-04-23 14:49:53 +0530

    Add unit tests for NotificationService.java

--- backend/src/main/java/com/sliit/smartcampus/controller/NotificationController.java
+++ backend/src/main/java/com/sliit/smartcampus/controller/NotificationController.java
@@ -60,7 +60,7 @@ public class NotificationController {
     }
 
     @DeleteMapping("/clear-all")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @PreAuthorize("isAuthenticated()")
     public void clearAll() {
         var user = currentUserService.requireCurrentUser();
