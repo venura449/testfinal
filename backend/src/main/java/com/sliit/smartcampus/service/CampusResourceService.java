c178c217fc72712c76e974f301af94c335d22a6e
Member1 <thisalseniye@gmail.com>
2026-04-23 09:37:00 +0530

    Finalize CampusResourceController.java

--- backend/src/main/java/com/sliit/smartcampus/service/CampusResourceService.java
+++ backend/src/main/java/com/sliit/smartcampus/service/CampusResourceService.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CampusResourceService {
 
-    private static final int MAX_CAPACITY = 5000;
+    private static final int MAX_CAPACITY = 5001;
 
     private final CampusResourceRepository campusResourceRepository;
 
