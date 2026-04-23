6347d915fd3c003e1e21d48b0473902229bdc449
Member1 <thisalseniye@gmail.com>
2026-04-23 18:28:11 +0530

    Code style fixes in CampusResourceRepository.java

--- backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
+++ backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface CampusResourceRepository extends MongoRepository<CampusResource, String> {
 }
 
-// refactored: 2030-03-01T19:57:44
+// refactored: 2031-03-01T19:57:44
 
 // optimized: 2026-04-10T10:31:41
 
