cb9db94deb9cca217a00e32926efa55fd3ae6634
Member3 <lansakara1020@gmail.com>
2026-04-23 12:59:23 +0530

    Handle null checks in MaintenanceController.java

--- backend/src/main/java/com/sliit/smartcampus/controller/MaintenanceController.java
+++ backend/src/main/java/com/sliit/smartcampus/controller/MaintenanceController.java
@@ -75,7 +75,7 @@ public class MaintenanceController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, "/comments/{commentId}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @PreAuthorize("isAuthenticated()")
     public void deleteComment(@PathVariable String commentId) {
         maintenanceService.deleteComment(commentId, currentUserService.requireCurrentUser());
